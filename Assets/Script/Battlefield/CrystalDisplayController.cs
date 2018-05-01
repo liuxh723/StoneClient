0976d287f7d8464b0d913569bc9568189122bb6d
liuxh723 <86610076@qq.com>
2018-05-01 21:13:12 +0800

    battlefield

---
+++ Assets/Script/Battlefield/CrystalDisplayController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrystalDisplayController : MonoBehaviour {
+
+    public List<GameObject> crystalList = new List<GameObject>();
+
+    public Sprite actve;
+    public Sprite used;
+
+
+	// Use this for initialization
+
+
+	void Start () {
+		for(int i=0;i<10;i++)
+        {
+            GameObject obj = transform.Find("Image (" + i.ToString() + ")").gameObject;
+            crystalList.Add(obj);
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetCrystalDisplay(int max,int activeNum)
+    {
+        Debug.LogFormat("count:{0}", crystalList.Count.ToString());
+        for(int i=0;i<10;i++)
+        {
+            if(i >= max)
+            { 
+            crystalList[i].GetComponent<Image>().enabled = false;
+            }
+            if(i >= activeNum)
+            {
+                crystalList[i].GetComponent<Image>().sprite = used;
+            }
+            else
+            {
+                crystalList[i].GetComponent<Image>().sprite = actve;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Battlefield/CrystalManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrystalManager : MonoBehaviour {
+
+    public CrystalDisplayController cdc;
+    public Text self;
+    public Text Animy;
+
+    private int selfMaxCry;
+    private int selfActiveCry;
+    private int aniMaxCry;
+    private int aniActiveCry;
+
+
+    private void Awake()
+    {
+        
+
+    }
+    // Use this for initialization
+    void Start () {
+
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetCrystal(int selfMax, int selfActive,int aniMax,int aniActive)
+    {
+        if(selfMax > -1)
+        {
+            selfMaxCry = selfMax;
+        }
+        if (selfActive > -1)
+        {
+            selfActiveCry = selfActive;
+        }
+        if (aniMax > -1)
+        {
+            aniMaxCry = aniMax;
+        }
+        if (aniActive > -1)
+        {
+            aniActiveCry = aniActive;
+        }
+
+
+    }
+
+    public void UpdataDisplay()
+    {
+        self.text = selfActiveCry.ToString() + "/" + selfMaxCry.ToString();
+        Animy.text = aniActiveCry.ToString() + "/" + aniMaxCry.ToString();
+        cdc.SetCrystalDisplay(selfMaxCry, selfActiveCry);
+    }
+
+    private void OnGUI()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            SetCrystal(8, 6, 7, 0);
+            UpdataDisplay();
+        }
+    }
+}
