2b754a70a2883c743ae2daa258da971f3a809313
liuxh723 <86610076@qq.com>
2018-05-02 22:18:41 +0800

    battlefield

---
+++ Assets/Script/Battlefield/Follower/FollowerManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerManager : MonoBehaviour {
+
+    public GameObject FollowerPrefab;
+    public List<GameObject> FollowerList = new List<GameObject>();
+
+    private float weight;
+    private float height;
+
+    public BaseEntity AddFollower()
+    {
+        GameObject obj = Instantiate(FollowerPrefab);
+        FollowerList.Add(obj);
+        obj.transform.SetParent(transform);
+        UpdataFollowerlist();
+        return obj.GetComponent<SingleFollowerController>();
+    }
+
+    public void UpdataFollowerlist()
+    {
+        for (int i = 0; i < FollowerList.Count; i++)
+        {
+            //Cards[i].transform.Rotate(0, 0, 30-i*6);
+            FollowerList[i].transform.position = transform.position + new Vector3(-(FollowerList.Count/2.0f-i-0.5f)* weight, 0, 0);
+
+        }
+    }
+    public void RemoveFollower(BaseEntity sfc)
+    {
+        GameObject tagObj = null;
+
+        foreach (GameObject obj in FollowerList)
+        {
+            if (obj.GetComponent<SingleFollowerController>() == sfc)
+            {
+                tagObj = obj;
+            }
+        }
+        if (tagObj != null)
+        {
+            FollowerList.Remove(tagObj);
+            Destroy(tagObj);
+            UpdataFollowerlist();
+
+        }
+        else
+        {
+            Debug.Log("移除手牌失败");
+        }
+
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        weight = GetComponent<RectTransform>().rect.size.x/7;
+        height = GetComponent<RectTransform>().rect.size.y;
+        AddFollower();
+        AddFollower();
+        AddFollower();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Script/Battlefield/HandCards/HandCardsListController.cs
+++ Assets/Script/Battlefield/HandCards/HandCardsListController.cs
@@ -6,16 +6,68 @@ using UnityEngine.UI;
 
 public class HandCardsListController : MonoBehaviour {
 
-    public GameObject[] Cards;
+    public List<GameObject> Cardslist = new List<GameObject>();
+
+    public GameObject cardPrefab;
+
+    public Transform[] listArea;
+    public Transform useCardPoint;
+
+    public float xOffset;
+
+    public void UpdataHandCardlist()
+    {
+        for (int i = 0; i < Cardslist.Count; i++)
+        {
+            //Cards[i].transform.Rotate(0, 0, 30-i*6);
+            Cardslist[i].transform.position = listArea[0].position + new Vector3(xOffset*i, 0, 0);
+
+        }
+    }
+    
+    public BaseEntity AddCard()
+    {
+        GameObject obj = Instantiate(cardPrefab);
+        Cardslist.Add(obj);
+        obj.transform.SetParent(transform);
+        UpdataHandCardlist();
+        return obj.GetComponent<SingleHandCardController>();
+    }
+
+    public void RemoveCard(BaseEntity shcc)
+    {
+        GameObject tagObj = null;
+
+        foreach(GameObject obj in Cardslist)
+        {
+            if(obj.GetComponent<SingleHandCardController>() == shcc)
+            {
+                tagObj = obj;
+            }
+        }
+        if(tagObj != null)
+        {
+            Cardslist.Remove(tagObj);
+            Destroy(tagObj);
+            UpdataHandCardlist();
+
+        }
+        else
+        {
+            Debug.Log("移除手牌失败");
+        }
+
+
+    }
 	// Use this for initialization
 	void Start () {
 
-        for(int i = 0;i<10;i++)
-        {
-            //Cards[i].transform.Rotate(0, 0, 30-i*6);
-            Cards[i].transform.position = new Vector3(140+i * 30, 160,0);
+        xOffset = (listArea[1].position.x - listArea[0].position.x) / 9;
 
-        }
+        AddCard();
+        AddCard();
+        AddCard();
+        AddCard();
         //for (int j = 5; j < 10; j++)
         //{
         //    Cards[j].transform.Rotate(0, 0, -(j-4) * 6);
