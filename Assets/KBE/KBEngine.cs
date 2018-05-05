0a2cff2ea775f14fd329c2f6684b89aae57032b1
liuxh723 <86610076@qq.com>
2018-05-05 19:40:32 +0800

    card

--- Assets/KBE/KBEngine.cs
+++ Assets/KBE/KBEngine.cs
@@ -81,7 +81,7 @@
 		public string serverScriptVersion = "";
 		public string clientScriptVersion = "0.1.0";
 		public string serverProtocolMD5 = "F217117F928F68467C45B47BA31B1B8B";
-		public string serverEntitydefMD5 = "392396FFB75CFEDA4A20A9B39202276F";
+		public string serverEntitydefMD5 = "B143AB9B582BE7E89AADB473BF87FCD9";
 		
 		// 当前玩家的实体id与实体类别
 		public UInt64 entity_uuid = 0;
